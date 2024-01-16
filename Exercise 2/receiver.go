1b08b398e11391eeb3307c90dd61b70afaf896d0
Sorstronen <snorrehs@stud.ntnu.no>
2024-01-16 13:41:37 +0100

    Added comments in receiver.go

--- Exercise 2/receiver.go
+++ Exercise 2/receiver.go
@@ -7,20 +7,23 @@ import (
 
 func main() {
 	// Specify the address to listen on (including port)
-	address := ":30000"
-	udpAddr, err := net.ResolveUDPAddr("udp", address)
-	if err != nil {
+	address := "10.100.23.129"                         //":30000" //Opprett streng med IP addresse (eller portnummer, begge funker)
+	udpAddr, err := net.ResolveUDPAddr("udp", address) //finner UDP addresses tilknyttet IP addressen/portnummeret
+
+	if err != nil { //evt si ifra hvis noe gikk galt
 		fmt.Println("Error resolving UDP address:", err)
 		return
 	}
 
 	// Create a UDP connection
-	conn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
+	conn, err := net.ListenUDP("udp", udpAddr) //returnerer enn net.UDPConn variabel som kan sende og receive fra UDP addressen
+
+	if err != nil { //blablabla feilhåndtering
 		fmt.Println("Error listening on UDP:", err)
 		return
 	}
-	defer conn.Close()
+	defer conn.Close() //defer sier at en funksjon, i dette tilfellet conn.Close() som dreper UDPConn-en vår,
+	//skal utføres når funksjonen den er i, i dette tilfellet main, er ferdig
 
 	fmt.Println("UDP Server listening on", address)
 
