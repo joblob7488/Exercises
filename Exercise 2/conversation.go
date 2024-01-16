340568ec1473296a20c28b6995407842afb6777e
Sorstronen <snorrehs@stud.ntnu.no>
2024-01-16 15:57:38 +0100

    IDK

--- Exercise 2/conversation.go
+++ Exercise 2/conversation.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"net"
-	"runtime"
 )
 
-func receiver(convChan chan string){
+func receiver() {
 	// Specify the address to listen on (including port)
-	recAddress := ":20013" //Opprett streng med IP addresse (eller portnummer, begge funker)
+	recAddress := ":20013"                                //Opprett streng med IP addresse (eller portnummer, begge funker)
 	udpAddr, err := net.ResolveUDPAddr("udp", recAddress) //finner UDP addresses tilknyttet IP addressen/portnummeret
 
 	if err != nil { //evt si ifra hvis noe gikk galt
@@ -18,23 +17,22 @@ func receiver(convChan chan string){
 
 	// Create a UDP connection
 	recConn, err := net.ListenUDP("udp", udpAddr) //returnerer enn net.UDPConn variabel som kan sende og receive fra UDP addressen
-	if err != nil { //blablabla feilhåndtering
+	if err != nil {                               //blablabla feilhåndtering
 		fmt.Println("Error listening on UDP:", err)
 		return
 	}
 
-
-	defer recConn.Close() //defer sier at en funksjon, i dette tilfellet conn.Close() som dreper UDPConn-en vår,
+	//defer recConn.Close() //defer sier at en funksjon, i dette tilfellet conn.Close() som dreper UDPConn-en vår,
 	//skal utføres når funksjonen den er i, i dette tilfellet receiver, er ferdig
 
 	fmt.Println("UDP Server listening on", recAddress)
 
 	// Buffer to hold incoming data
 	buffer := make([]byte, 1024)
-	
+
 	for {
 		// Read data from the connection
-		n, clientAddr, err := recConn.ReadFromUDP(buffer) 
+		n, clientAddr, err := recConn.ReadFromUDP(buffer)
 		if err != nil {
 			fmt.Println("Error reading from UDP:", err)
 			continue
@@ -43,12 +41,12 @@ func receiver(convChan chan string){
 		// Process the received data
 		data := buffer[:n]
 		fmt.Printf("Received from %v: %s\n", clientAddr, data)
-		convChan <- ""
+
 	}
 }
 
-func sender(){
-	sendAddress := "20013"
+func sender() {
+	sendAddress := ":20013"
 	serverAddr, err := net.ResolveUDPAddr("udp", sendAddress)
 
 	if err != nil {
@@ -75,15 +73,12 @@ func sender(){
 	}
 
 	fmt.Println("Message sent to", serverAddr)
-	convChan <- 
+
 }
 
-
 func main() {
-	runtime.GOMAXPROCS(2)
+	//runtime.GOMAXPROCS(2)
 
-	convChan := make(chan string)
-
-	go sender(convChan)
-	go receiver(convChan)
+	go sender()
+	go receiver()
 }
