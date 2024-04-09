71d0ae0834ad182babfbb896ad7478f70faa8798
heimsnik <cmheimvik@gmail.com>
2024-04-09 12:19:41 +0200

    Added 5

--- Exercise 2/Network-go/coms.go
+++ Exercise 2/Network-go/coms.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	//"Network-go/network/bcast"
-	//"Network-go/network/localip"
 	"Network-go/network/bcast"
-	"Network-go/network/peers"
-	//"fmt"
-	//"os"
-	//"time"
+	"Network-go/network/localip"
+	"flag"
+	"fmt"
+	"os"
 )
 
 type msg struct {
-	msg    string
-	number uint
+	text string
+	//number uint
 }
 
 type con_status int
@@ -59,19 +57,38 @@ func recieve(ch chan string, port int) {
 }
 
 func main() {
+	//Give each one ID
+	var id string
+	flag.StringVar(&id, "id", "", "id of this peer")
+	flag.Parse()
+
+	// ... or alternatively, we can use the local IP address.
+	// (But since we can run multiple programs on the same PC, we also append the
+	//  process ID)
+	if id == "" {
+		localIP, err := localip.LocalIP()
+		if err != nil {
+			fmt.Println(err)
+			localIP = "DISCONNECTED"
+		}
+		id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
+	}
 
 	//Check for active nodes on ip:port
-	peerUpdateCh := make(chan peers.PeerUpdate)
-	go peers.Receiver(30000, peerUpdateCh)
+	//peerUpdateCh := make(chan peers.PeerUpdate)
+	//go peers.Receiver(30000, peerUpdateCh)
 
 	//define the datatype we want to recive
-	rx_ch := make(chan string)
+	rx_ch := make(chan msg)
 
 	//recieve thread
-	go recieve(rx_ch, 30000)
+	go bcast.Receiver(30000, rx_ch) //recieve(rx_ch, 30000)
 
 	//printing loop
 	for {
-		println(<-rx_ch)
+		select {
+		case in := <-rx_ch:
+			println("Received:  %#v\n", in.text)
+		}
 	}
 }
--- Exercise 2/Network-go/main.go
+++ Exercise 2/Network-go/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"Network-go/network/bcast"
-	"Network-go/network/localip"
-	"Network-go/network/peers"
-	"flag"
+	//"Network-go/network/localip"
+	//"Network-go/network/peers"
+	//"flag"
 	"fmt"
-	"os"
+	//"os"
 	"time"
 )
 
@@ -22,13 +22,14 @@ type HelloMsg struct {
 func main() {
 	// Our id can be anything. Here we pass it on the command line, using
 	//  `go run main.go -id=our_id`
-	var id string
-	flag.StringVar(&id, "id", "", "id of this peer")
-	flag.Parse()
+	//var id string
+	//flag.StringVar(&id, "id", "", "id of this peer")
+	//flag.Parse()
 
 	// ... or alternatively, we can use the local IP address.
 	// (But since we can run multiple programs on the same PC, we also append the
 	//  process ID)
+	/*
 	if id == "" {
 		localIP, err := localip.LocalIP()
 		if err != nil {
@@ -37,15 +38,15 @@ func main() {
 		}
 		id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
 	}
-
+	*/
 	// We make a channel for receiving updates on the id's of the peers that are
 	//  alive on the network
-	peerUpdateCh := make(chan peers.PeerUpdate)
+	///peerUpdateCh := make(chan peers.PeerUpdate)
 	// We can disable/enable the transmitter after it has been started.
 	// This could be used to signal that we are somehow "unavailable".
-	peerTxEnable := make(chan bool)
-	go peers.Transmitter(15647, id, peerTxEnable)
-	go peers.Receiver(15647, peerUpdateCh)
+	//peerTxEnable := make(chan bool)
+	//go peers.Transmitter(15647, id, peerTxEnable)
+	//go peers.Receiver(15647, peerUpdateCh)
 
 	// We make channels for sending and receiving our custom data types
 	helloTx := make(chan HelloMsg)
@@ -58,7 +59,7 @@ func main() {
 
 	// The example message. We just send one of these every second.
 	go func() {
-		helloMsg := HelloMsg{"Hello from " + id, 0}
+		helloMsg := HelloMsg{"Hello from ", 0}
 		for {
 			helloMsg.Iter++
 			helloTx <- helloMsg
@@ -69,11 +70,6 @@ func main() {
 	fmt.Println("Started")
 	for {
 		select {
-		case p := <-peerUpdateCh:
-			fmt.Printf("Peer update:\n")
-			fmt.Printf("  Peers:    %q\n", p.Peers)
-			fmt.Printf("  New:      %q\n", p.New)
-			fmt.Printf("  Lost:     %q\n", p.Lost)
 
 		case a := <-helloRx:
 			fmt.Printf("Received: %#v\n", a)
